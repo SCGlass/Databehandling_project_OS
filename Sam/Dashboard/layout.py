6ca08530384c2bb3222aa355112dc1cbcd1db10a
Sam Glass <sam.charles.glass@gmail.com>
2022-11-18 23:06:49 +0100

    trying to do dashboard

--- Sam/Dashboard/layout.py
+++ Sam/Dashboard/layout.py
@@ -5,9 +5,12 @@ import dash_bootstrap_components as dbc
 class Layout:
     def layout(self):
         return dbc.Container(
-
-            dbc.Card(
-                dbc.CardBody(html.H1("Techy stocks viewer")), className={"mt-3"})
-
-
-        )
+            [
+                dbc.Card(
+                    dbc.CardBody
+                        ((html.H1("Olympic Games Dashboard")),
+                        (html.Img(src="assets/olympic-logo.png"))), className="rounded float-right")
+                        
+                        
+                        ]
+                    )
