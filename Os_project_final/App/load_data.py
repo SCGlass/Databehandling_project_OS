0507c4d897793e3766494e27dedee8a78e277756
Hakan Akerblom <hakan.akerblom@gmail.com>
2022-11-21 13:17:52 +0100

    mapplots

--- Os_project_final/App/load_data.py
+++ Os_project_final/App/load_data.py
@@ -22,3 +22,39 @@ def top_10_medals():
 
     top_10 = medal_counts.sort_values(by = "Gold", ascending=False).head(10)
     return px.bar(top_10, barmode="group")
+
+def map_medals_Art_Competitions():
+
+    data_folder = os.path.abspath("./Data")
+    data_athletes = os.path.join(data_folder, "athlete_events.csv")
+    df = pd.read_csv(data_athletes)
+
+    df1 = df[df["Sport"] == "Art Competitions"]
+    df1 = df1[df1.Medal.notna()]
+    df1 = df1.groupby("NOC").Medal.count().reset_index(name="Total")
+    df1
+    fig = px.choropleth(df1, locations="NOC", locationmode= "ISO-3",
+                        color="Total",
+                        hover_name="NOC",
+                        color_continuous_scale="Viridis",
+                        title="Art competition - number of medals")
+    
+    return fig
+
+def map_medals_Ice_Hockey():
+
+    data_folder = os.path.abspath("./Data")
+    data_athletes = os.path.join(data_folder, "athlete_events.csv")
+    df = pd.read_csv(data_athletes)
+
+    df1 = df[df["Sport"] == "Ice Hockey"]
+    df1 = df1[df1.Medal.notna()]
+    df1 = df1.groupby("NOC").Medal.count().reset_index(name="Total")
+    df1
+    fig = px.choropleth(df1, locations="NOC", locationmode= "ISO-3",
+                        color="Total",
+                        hover_name="NOC",
+                        color_continuous_scale="Viridis",
+                        title="Ice Hockey - number of medals")
+    
+    return fig
