e0883137b2ff3a4b5c39d71a4bbf989af40aca22
Sam Glass <sam.charles.glass@gmail.com>
2022-11-21 11:39:25 +0100

    layout continued

--- Os_project_final/App/layout.py
+++ Os_project_final/App/layout.py
@@ -5,7 +5,8 @@ import dash_bootstrap_components as dbc
 class Layout:
     def layout(self):
         return dbc.Container([
-             html.Div([
+            html.Div(children=[
+            html.H1(children='Olympic Dashboard statistics'),
 
         html.Div([
             dcc.Dropdown(
