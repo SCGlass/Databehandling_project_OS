ed7f1c180fd1b1d3fd100aa2a4811c011da34959
Hakan Akerblom <hakan.akerblom@gmail.com>
2022-11-21 14:28:07 +0100

    choropleth

--- Os_project_final/App/load_data.py
+++ Os_project_final/App/load_data.py
@@ -7,16 +7,12 @@ def top_10_medals():
     data_folder = os.path.abspath("./Data")
     data_athletes = os.path.join(data_folder, "athlete_events.csv")
     df = pd.read_csv(data_athletes)
-
     df_gold = df[df.Medal == "Gold"]
     gold_series = df_gold.groupby("NOC").Medal.count().sort_values(ascending=False)
-
     df_silver = df[df.Medal == "Silver"]
     silver_series = df_silver.groupby("NOC").Medal.count().sort_values(ascending=False)
-
     df_bronze = df[df.Medal == "Bronze"]
     bronze_series = df_bronze.groupby("NOC").Medal.count().sort_values(ascending=False)
-
     medal_counts = pd.DataFrame({"Gold": gold_series, "Silver": silver_series, "Bronze": bronze_series})
     medal_counts.sort_values(by = "Gold", ascending=False).head(20)
 
@@ -32,12 +28,19 @@ def map_medals_Art_Competitions():
     df1 = df[df["Sport"] == "Art Competitions"]
     df1 = df1[df1.Medal.notna()]
     df1 = df1.groupby("NOC").Medal.count().reset_index(name="Total")
-    df1
-    fig = px.choropleth(df1, locations="NOC", locationmode= "ISO-3",
+
+    df_iso = pd.read_html("https://www.worlddata.info/countrycodes.php")[0]
+    df_iso.set_index("IOC", inplace=True)
+    df_iso = df_iso.reset_index()
+    df_iso = df_iso.rename(columns={"IOC": "NOC"})
+    df_iso = df_iso[["NOC", "ISO 3166-1 alpha3"]]
+    df_merge = df1.merge(df_iso)
+
+    fig = px.choropleth(df_merge, locations="ISO 3166-1 alpha3", locationmode= "ISO-3",
                         color="Total",
                         hover_name="NOC",
                         color_continuous_scale="Viridis",
-                        title="Art competition - number of medals")
+                        title="Art Competition - medals per country")
     
     return fig
 
@@ -50,11 +53,18 @@ def map_medals_Ice_Hockey():
     df1 = df[df["Sport"] == "Ice Hockey"]
     df1 = df1[df1.Medal.notna()]
     df1 = df1.groupby("NOC").Medal.count().reset_index(name="Total")
-    df1
-    fig = px.choropleth(df1, locations="NOC", locationmode= "ISO-3",
+
+    df_iso = pd.read_html("https://www.worlddata.info/countrycodes.php")[0]
+    df_iso.set_index("IOC", inplace=True)
+    df_iso = df_iso.reset_index()
+    df_iso = df_iso.rename(columns={"IOC": "NOC"})
+    df_iso = df_iso[["NOC", "ISO 3166-1 alpha3"]]
+    df_merge = df1.merge(df_iso)
+
+    fig = px.choropleth(df_merge, locations="ISO 3166-1 alpha3", locationmode= "ISO-3",
                         color="Total",
                         hover_name="NOC",
                         color_continuous_scale="Viridis",
-                        title="Ice Hockey - number of medals")
+                        title="Ice Hockey - medals per country")
     
     return fig
