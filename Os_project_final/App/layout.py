6de2934179ad85501e55232dd3f0f1b37e2a425f
Sam Glass <sam.charles.glass@gmail.com>
2022-11-22 22:58:41 +0100

    final touches with dashboard

--- Os_project_final/App/layout.py
+++ Os_project_final/App/layout.py
@@ -1,6 +1,6 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-from dash_bootstrap_templates import load_figure_template
+
 
 
 class Layout:
@@ -9,37 +9,36 @@ class Layout:
         self.GB_stats = [
             {"label": "Sports Great Britain won the most medals in", "value": "medals"},
             {"label": "Amount of medals won per Olympic Games", "value": "medals_os"},
-            {"label": "Age distribution of Athletes representing Great Britain", "value": "age"}]
+            {"label": "Age distribution of Athletes representing Great Britain", "value": "age"},
+            {"label": "Height and Weight averages of Different sports in Great Britain Team", "value":"height_weight"}
+        ]
 
         self.world_stats = [
-            {"label": "Top Highest Ten countries medal count",
-                "value": "top_ten_medals"},
+            {"label": "Top ranking countries medal count", "value": "top_ten_medals"},
             {"label": "Distribution of Winners in Art competitions", "value": "art_comp"},
             {"label": "Distribution of Winners in Ice Hockey", "value": "ice_hockey"}
         ]
 
     def layout(self):
-        load_figure_template('UX')
+        
         return dbc.Container([
+             
 
-            html.Div(children=[
-                html.H1(children='Olympic Dashboard statistics'),
-                style={'background-image': 'url(/assets/image.jpg)',
-                        'background-size': '100%',
-                        'position': 'fixed',
-                        'width': '100%',
-                        'height': '100%'}
+            html.Div(
+                children=[
+                html.H1(children='Olympic Games Statistics'),
+                
 
 
                 dcc.Tabs([
-                    dcc.Tab(label='Team Great Britain', children=[
+                    dcc.Tab(label='Team Great Britain',children=[
                             dcc.Dropdown(
                                 id='gb-dropdown',
                                 options=self.GB_stats,
                                 value="medals"
                             ),
                             dcc.Graph(id='graph'),
-                            ], style={'width': '48%', 'display': 'inline-block'}),
+                            ], style={'width': '50%', 'display': 'inline-block'}),
 
                     dcc.Tab(label='World Olympics', children=[
 
@@ -49,11 +48,10 @@ class Layout:
                             value="top_ten_medals"
                         ),
                         dcc.Graph(id='graph-down')
-                    ], style={'width': '48%', 'display': 'inline-block'}),
+                    ], style={'width': '50%', 'display': 'inline-block'}),
 
 
                 ]),
-
 
 
 
